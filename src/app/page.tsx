ec7b2ee80aa832ae2117991190803d4726e4ee5f
ken314526 <abhisheksharma801831@gmail.com>
2024-04-29 21:53:25 +0530

    Changes 29-04-2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import CarouselCards from "@/components/CarouselCards";
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <CarouselCards />
+      <UpcomingWebinars />
     </main>
   );
 }
